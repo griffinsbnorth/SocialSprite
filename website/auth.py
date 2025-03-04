206254c6a82143e42eebc875b22d407a44ee6a63
SkullCat <satara.cressy@gmail.com>
2025-03-04 04:42:23 -0700

    Adding settings page

--- website/auth.py
+++ website/auth.py
@@ -10,3 +10,8 @@ def login():
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
+
+@auth.route('/settings', methods=['GET', 'POST'])
+def settings():
+    data = request.form
+    return render_template("settings.html")
